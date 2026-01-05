a35dfd65027e0469c8748ab2c808e3eef25a36a4
Sulochan Mahajan <sulochanmahajan@gmail.com>
2026-01-05 16:53:03 +0530

    origin updated

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173",
+                "https://taskilo-wheat.vercel.app"
+        ],
         credentials: true,
     })
 );
